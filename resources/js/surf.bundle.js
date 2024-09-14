6a2282617a37f7dfa09503f9b24b9bc7656c0de3
seethathuman <166908340+seethathuman@users.noreply.github.com>
2024-09-15 00:26:20 +0800

    Update surf.bundle.js

--- resources/js/surf.bundle.js
+++ resources/js/surf.bundle.js
@@ -10609,13 +10609,14 @@
                     .settingsHamburgerMenu_flyout_divider,
               }),
 
-              // game settings toggles
+              /** game settings toggles
               this.renderToggleSection(),
               a.createElement(P.iz, {
                 className:
                   this.props.managedClasses
                     .settingsHamburgerMenu_flyout_divider,
               }),
+              */
 
               // game tips, info, and reset
               this.renderButtonSection()
