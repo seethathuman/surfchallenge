dd56493b8b595b462d800fb787a2f3848e44ae11
seethathuman <166908340+seethathuman@users.noreply.github.com>
2024-09-17 17:13:14 +0800

    Update surf.bundle.js

--- resources/js/surf.bundle.js
+++ resources/js/surf.bundle.js
@@ -8381,10 +8381,10 @@
                     Ae.sys.routeInput(He.Down);
                     break;
                   case "left":
-                    Ae.sys.routeInput(He.Left, !1, !1);
+                    Ae.sys.routeInput(He.Left);
                     break;
                   case "right":
-                    Ae.sys.routeInput(He.Right, !1, !1);
+                    Ae.sys.routeInput(He.Right);
                     break;
                   case "select":
                   case "start":
